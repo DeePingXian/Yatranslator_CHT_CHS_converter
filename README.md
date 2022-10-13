ee4729d2d04a7e71b0e2e5626b476d133cf90f35
DeePingXian <lance8819@gmail.com>
2022-10-13 21:37:09 +0800

    v1.3.0
    支援CPU多核並行轉換
    單行只有換行字元時不再提示轉換錯誤
    修正文本

--- README.md
+++ README.md
@@ -2,23 +2,37 @@
 
 ## 簡介
 本項目基於OpenCC開放中文轉換<br>
-此程式可以把Yatranslator翻譯文本每行的後段（即譯文），進行繁簡互轉，且OpenCC支援大陸台灣慣用語轉換，所以也一並做了進去。<br>
-Yatranslator翻譯文本的格式：TAB分隔日中文本、換行區隔翻譯項目。<br><br>
+由於幾乎所有COM3D2的民間中譯文本都是簡體的，所以做了這程式出來，方便轉成繁體<br>
+此程式可以把Yatranslator翻譯文本每行的後段（即譯文），進行繁簡互轉，且OpenCC支援大陸台灣及日常慣用語轉換，所以也一並做了進去<br>
+Yatranslator翻譯文本的格式：TAB分隔日中文本、換行區隔翻譯項目<br><br>
 
 ## 使用說明
-※放置此程式的資料夾路徑只能包含英數，不能有中文等其他語言，以降低出錯機率<br>
-使用時將要轉換的文本置於與「converter.exe」同一目錄的「before」資料夾下（沒有就創建一個），可包含資料夾，但不支援zip，請先解壓縮。<br>
-轉換完的文本會自動置於「after」資料夾下，若該資料夾已存在，則會存於往後編號的的資料夾。<br>
-轉換錯誤的那行文本會直接照抄並提示，不進行轉換；若檔案不是txt檔的話會跳過並提示。<br>
-這程式面對COM3D2海量的文本，執行效率顯得十分低落，實際使用時建議把文本拆成幾份，多開幾個程式一起轉換。<br>
-Releases那邊exe檔的編譯器：<br>
-- v1.0.0：PyInstaller
-- v1.2.0以後：Nuitka ＆ GCC
+放置此程式的資料夾路徑請盡量只包含英數，以降低出錯機率<br>
+使用時將要轉換的文本置於與「converter.exe」同一目錄的「before」資料夾下（沒有就創建一個），可包含資料夾，但不支援zip檔，請先解壓縮<br>
+轉換完的文本會自動置於「after」資料夾下，若該資料夾已存在，則會存於往後編號的的資料夾<br>
+轉換錯誤的那行文本會直接照抄並提示，不進行轉換；若檔案不是txt檔的話會跳過並提示<br>
+當一檔案的文本在51行以上時會使用多核並行轉換，以加快轉換速度，50行以下則不使用，避免分配工作消耗太多時間<br>
 
-實測效能<br>
-Intel® Core™ i5-9400F：約29行/s<br>
-AMD Ryzen™ 7 3700X PBO FCLK 1500MHz：約37行/s<br>
-AMD Ryzen™ 7 7700X PBO FCLK 2000MHz：約70行/s<br><br>
+***
+
+## 實測效能
+<table>
+<tr><td align="center" colspan = "2">單核簡轉繁（包含慣用語）</td></tr>
+<tr><td>CPU</td><td>效能</td></tr>
+<tr><td>Intel® Core™ i5-9400F</td><td>約29.5行/s</td></tr>
+<tr><td>AMD Ryzen™ 7 3700X PBO FCLK 1500MHz</td><td>約37行/s</td></tr>
+<tr><td>AMD Ryzen™ 7 3700X @4.2GHz FCLK 1833MHz</td><td>約37行/s</td></tr>
+<tr><td>AMD Ryzen™ 7 7700X PBO FCLK 2000MHz</td><td>約70行/s</td></tr>
+</table><table>
+<tr><td align="center" colspan = "2">多核簡轉繁（包含慣用語）</td></tr>
+<tr><td>CPU</td><td>效能</td></tr>
+<tr><td>Intel® Core™ i5-9400F</td><td>約122行/s</td></tr>
+<tr><td>AMD Ryzen™ 7 3700X PBO FCLK 1500MHz</td><td>約300行/s</td></tr>
+<tr><td>AMD Ryzen™ 7 3700X @4.2GHz FCLK 1833MHz</td><td>約330行/s</td></tr>
+<tr><td>AMD Ryzen™ 7 7700X PBO FCLK 2000MHz</td><td>約450行/s</td></tr>
+</table>
+
+***
 
 ## 使用規範
 此程式允許任何形式的使用、修改、打包、轉發，不需經本人同意，也可以加進自己的專案中。轉換它人的文本，除個人使用外之用途請經文本主人同意。
--- converter.py
+++ converter.py
@@ -1,6 +1,7 @@
 import os
 import msvcrt
 import json
+import multiprocessing
 from alive_progress import alive_bar
 from alive_progress.animations import scrolling_spinner_factory
 from opencc import OpenCC
@@ -11,80 +12,143 @@ with open('settings.json' , 'r' , encoding = 'utf8') as language:
     language = json.load(language)
 language = language['defaultLanguage']
 
-print(json_data[language]['intro']+'\n')
-mode = ''
-while mode not in ('1','2','3','4'):
-    mode = input(json_data[language]['inputMessage']+'\n')
-    if mode == '5':
-        if language == 'CHT':
-            language = 'CHS'
-            with open('settings.json' , 'w' , encoding = 'utf8') as f:
-                json.dump({'defaultLanguage': 'CHS'}, f, ensure_ascii=False)
-        elif language == 'CHS':
-            language = 'CHT'
-            with open('settings.json' , 'w' , encoding = 'utf8') as f:
-                json.dump({'defaultLanguage': 'CHT'}, f, ensure_ascii=False)
+def converter(line , mode , lineNum , lineQueue):
+    try:
+        lineQueue.put((lineNum , line.split('\t')[0]+'\t'+OpenCC(mode).convert(line.split('\t')[1]) , False))
+    except:
+        lineQueue.put((lineNum , line , True))
+    return 0
 
-if 'before' in os.listdir():
-    if mode == '1':
-        mode = 's2t'
-    elif mode == '2':
-        mode = 't2s'
-    elif mode == '3':
-        mode = 's2twp'
-    else:
-        mode = 'tw2sp'
-    targetFolder = 'after'      #決定after資料夾編號
-    if targetFolder in os.listdir():
-        targetFolder = 'after_2'
-    while targetFolder in os.listdir():
-        targetFolder = f'after_{int((targetFolder.partition("_"))[2])+1}'
-
-    print()     #換行
+def writer(file , lineQueue , lineSum , fileNum , fileSum):
+    #lineDict = {第幾行int:(文字str,是否出錯bool)}      用字典讓各行輸出的順序維持原檔案的樣子，不會被並行處理打亂
+    lineDict = {}
+    lineNum = 1
+    lineReceived = 0
     spinner = scrolling_spinner_factory(json_data[language]['convertedLines'], length=20 , background='-', right=True, hide=True, wrap=True, overlay=False)        #自訂進度條動畫
-    fileSum = 0
-    for dirs , subdirs , files in os.walk('before' , topdown=True):
-        if files:
+    with open(file , 'a' , encoding='UTF-8') as output_text_file:
+        with alive_bar(lineSum , title=f'{json_data[language]["file"]}{fileNum}/{fileSum}' , spinner=spinner , bar='smooth') as bar:
+            while True:
+                if lineReceived < lineSum:
+                    line = lineQueue.get()
+                    lineReceived += 1
+                while lineNum in lineDict:
+                    output_text_file.write(lineDict[lineNum][0])
+                    output_text_file.flush()
+                    if lineDict[lineNum][1] == True:
+                        if lineDict[lineNum][0] != '\n':
+                            print('\t'+json_data[language]['convertErrorFormer']+'「'+lineDict[lineNum][0].rstrip('\n')+'」，'+json_data[language]['convertErrorLatter'])
+                    del lineDict[lineNum]
+                    lineNum += 1
+                if line[0] == lineNum:
+                    output_text_file.write(line[1])
+                    output_text_file.flush()
+                    if line[2] == True:
+                        if line[1] != '\n':
+                            print('\t'+json_data[language]['convertErrorFormer']+'「'+line[1].rstrip('\n')+'」，'+json_data[language]['convertErrorLatter'])
+                    lineNum += 1
+                    bar()
+                elif line[0] > 0 and lineNum <= lineSum:
+                    lineDict[line[0]] = (line[1] , line[2])
+                    bar()
+                else:
+                    return 0
+
+if __name__ == '__main__':
+    CPUThreads = multiprocessing.cpu_count()        #一開始先抓CPU執行緒數，之後就不用再抓
+    if CPUThreads < 2:
+        CPUThreads = 2
+    print(json_data[language]['intro']+'\n')
+    mode = ''
+    while mode not in ('1','2','3','4'):
+        mode = input(json_data[language]['inputMessage']+'\n')
+        if mode == '5':
+            if language == 'CHT':
+                language = 'CHS'
+                with open('settings.json' , 'w' , encoding = 'utf8') as f:
+                    json.dump({'defaultLanguage': 'CHS'}, f, ensure_ascii=False)
+            elif language == 'CHS':
+                language = 'CHT'
+                with open('settings.json' , 'w' , encoding = 'utf8') as f:
+                    json.dump({'defaultLanguage': 'CHT'}, f, ensure_ascii=False)
+
+    if 'before' in os.listdir():
+        if mode == '1':
+            mode = 's2twp'
+        elif mode == '2':
+            mode = 'tw2sp'
+        elif mode == '3':
+            mode = 's2t'
+        else:
+            mode = 't2s'
+        targetFolder = 'after'      #決定after資料夾編號
+        if targetFolder in os.listdir():
+            targetFolder = 'after_2'
+        while targetFolder in os.listdir():
+            targetFolder = f'after_{int((targetFolder.partition("_"))[2])+1}'
+
+        print()     #換行
+        fileSum = 0
+        for dirs , subdirs , files in os.walk('before' , topdown=True):
+            if files:
+                for file in files:
+                    if file.endswith('.txt'):
+                        fileSum += 1       #數檔案總數
+
+        fileNum = 0
+        for dirs , subdirs , files in os.walk('before' , topdown=True):
+            if not os.path.isdir(targetFolder+dirs.lstrip('before')):
+                os.makedirs(targetFolder+dirs.lstrip('before'))     #產生目標資料夾
             for file in files:
                 if file.endswith('.txt'):
-                    fileSum += 1       #數檔案總數
-
-    fileNum = 0
-    for dirs , subdirs , files in os.walk('before' , topdown=True):
-        if not os.path.isdir(targetFolder+dirs.lstrip('before')):
-            os.makedirs(targetFolder+dirs.lstrip('before'))     #產生目標資料夾
-        for file in files:
-            if file.endswith('.txt'):
-                fileNum += 1
-                isEmptyFile = False
-                with open(dirs+'\\'+file , 'r' , encoding='UTF-8') as input_text_file_:
-                    if input_text_file_.readlines() == []:
-                        isEmptyFile = True
-                with open(dirs+'\\'+file , 'r' , encoding='UTF-8') as input_text_file:
-                    if not isEmptyFile:
-                        print('目前'+json_data[language]['file']+'：'+(dirs+'\\'+file).lstrip('before\\'))
-                        lineSum = list(enumerate(open(dirs+'\\'+file , 'r' , encoding='UTF-8') , start=1))[-1][0]       #數此txt檔的行數
-                        with alive_bar(lineSum , title=f'{json_data[language]["file"]}{fileNum}/{fileSum}' , spinner=spinner , bar='smooth') as bar:
-                            for line in input_text_file:
-                                with open(targetFolder+((dirs+'\\'+file).lstrip('before')) , 'a' , encoding='UTF-8') as output_text_file:
-                                    try:
-                                        output_text_file.write((line.split('\t'))[0]+'\t'+OpenCC(mode).convert(line.split('\t')[1]))
-                                    except:
-                                        output_text_file.write(line)
-                                        print('\t'+json_data[language]['convertErrorFormer']+'「'+line.rstrip('\n')+'」，'+json_data[language]['convertErrorLatter'])
-                                bar()
-                    else:
-                        print('「'+(dirs+'\\'+file).lstrip('before\\')+'」'+json_data[language]['emptyFile'])
-                        with open(targetFolder+((dirs+'\\'+file).lstrip('before')) , 'a' , encoding='UTF-8') as output_text_file:
-                            output_text_file.write('')
-            else:
-                print(json_data[language]['nonTxtError']+'：'+(dirs+'\\'+file).lstrip('before\\'))
-    print(json_data[language]['completed'])
-    while True:
-        if msvcrt.getch():
-            break
-else:
-    print(json_data[language]['nonBeforeFolderError'])
-    while True:
-        if msvcrt.getch():
-            break
+                    fileNum += 1
+                    isEmptyFile = False
+                    with open(dirs+'\\'+file , 'r' , encoding='UTF-8') as input_text_file_:
+                        if input_text_file_.readlines() == []:
+                            isEmptyFile = True
+                    with open(dirs+'\\'+file , 'r' , encoding='UTF-8') as input_text_file:
+                        if not isEmptyFile:
+                            print('目前'+json_data[language]['file']+'：'+(dirs+'\\'+file).lstrip('before\\'))
+                            lineSum = list(enumerate(open(dirs+'\\'+file , 'r' , encoding='UTF-8') , start=1))[-1][0]       #數此txt檔的行數
+                            if lineSum <= 50:
+                                spinner = scrolling_spinner_factory(json_data[language]['convertedLines'], length=20 , background='-', right=True, hide=True, wrap=True, overlay=False)        #自訂進度條動畫
+                                with alive_bar(lineSum , title=f'{json_data[language]["file"]}{fileNum}/{fileSum}' , spinner=spinner , bar='smooth') as bar:
+                                    for line in input_text_file:
+                                        with open(targetFolder+((dirs+'\\'+file).lstrip('before')) , 'a' , encoding='UTF-8') as output_text_file:
+                                            try:
+                                                output_text_file.write((line.split('\t'))[0]+'\t'+OpenCC(mode).convert(line.split('\t')[1]))
+                                                output_text_file.flush()
+                                            except:
+                                                output_text_file.write(line)
+                                                output_text_file.flush()
+                                                if line != '\n':
+                                                    print('\t'+json_data[language]['convertErrorFormer']+'「'+line.rstrip('\n')+'」，'+json_data[language]['convertErrorLatter'])
+                                        bar()
+                            else:
+                                lineNum = 1
+                                lineQueue = multiprocessing.Manager().Queue()
+                                taskPool = multiprocessing.Pool(processes = CPUThreads)
+                                taskList = []
+                                writer_ = taskPool.apply_async(writer , (targetFolder+((dirs+'\\'+file).lstrip('before')) , lineQueue , lineSum , fileNum , fileSum))
+                                for line in input_text_file:
+                                    task = taskPool.apply_async(converter , (line , mode , lineNum , lineQueue))
+                                    taskList.append(task)
+                                    lineNum += 1
+                                for task in taskList:
+                                    task.get()
+                                writer_.get()
+                                taskPool.close()
+                        else:
+                            print('「'+(dirs+'\\'+file).lstrip('before\\')+'」'+json_data[language]['emptyFile'])
+                            with open(targetFolder+((dirs+'\\'+file).lstrip('before')) , 'a' , encoding='UTF-8') as output_text_file:
+                                output_text_file.write('')
+                else:
+                    print(json_data[language]['nonTxtError']+'：'+(dirs+'\\'+file).lstrip('before\\'))
+        print(json_data[language]['completed'])
+        while True:
+            if msvcrt.getch():
+                break
+    else:
+        print(json_data[language]['nonBeforeFolderError'])
+        while True:
+            if msvcrt.getch():
+                break
